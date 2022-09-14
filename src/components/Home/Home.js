1422f3752fa33720a5814b772e2c22f65522dec7
olivia pasion <oap9820@gmail.com>
2022-09-14 13:02:06 -0700

    working on Font state in Preview.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -9,11 +9,12 @@ export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
   const [title, setTitle] = useState('Blog Title');
   const [subtitle, setSubtitle] = useState('Blog Subtitle');
+  const [font, setFont] = useState("Architect's Daughter");
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} subtitle={subtitle}/>
-      <Editor title={title} setTitle={setTitle} setSubtitle={setSubtitle} />
+      <Preview title={title} subtitle={subtitle} font={font}/>
+      <Editor title={title} setTitle={setTitle} subtitle={subtitle}setSubtitle={setSubtitle} font={font} setFont={setFont}/>
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title, subtitle }) {
+export default function Preview({ title, subtitle, font }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
@@ -13,10 +13,12 @@ export default function Preview({ title, subtitle }) {
   //   <h3> Blog Subtitle</h3>
   //   <p>This is some blog text. </p>
   // </div>
-  <div className="preview prop" style="text-align: center">
-    <h1>{title}</h1>
-    <h3>{subtitle}</h3>
-  </div>;
+  
 
-  return <></>;
+  return (
+    <div className="preview">
+      <h1>{title}</h1>
+      <h3>{subtitle}</h3>
+    </div>
+  );
 }
