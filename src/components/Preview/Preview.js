a620199bc4ea1bb9c5d25fd211ddae1d19255dfe
olivia pasion <oap9820@gmail.com>
2022-09-14 12:22:26 -0700

    Title state added to Main, Editor, and Preview js'

--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview() {
+export default function Preview({ title }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
@@ -13,6 +13,10 @@ export default function Preview() {
   //   <h3> Blog Subtitle</h3>
   //   <p>This is some blog text. </p>
   // </div>
+  <div className="preview prop" style="text-align: center">
+    <h1>{title}</h1>
+
+  </div>;
 
   return <></>;
 }
