f911c9d4028a5b34bfe1a2a5d2e7ef43cf5d5785
olivia pasion <oap9820@gmail.com>
2022-09-14 12:31:01 -0700

    Title state added to Main, Editor, and preview js'

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,12 +7,13 @@ import './Home.css';
 
 export default function Home() {
   // add useState calls here for title, subtitle, font, align, and text
-  const [title, setTitle] = useState('default state');
+  const [title, setTitle] = useState('Blog Title');
+  const [subtitle, setSubtitle] = useState('Blog Subtitle');
   return (
     <main>
       {/* pass the state variables as props to the presentational components */}
-      <Preview title={title} />
-      <Editor title={title} setTitle={setTitle} />
+      <Preview title={title} subtitle={subtitle}/>
+      <Editor title={title} setTitle={setTitle} setSubtitle={setSubtitle} />
     </main>
   );
 }
--- src/components/Preview/Preview.js
+++ src/components/Preview/Preview.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import './Preview.css';
 
-export default function Preview({ title }) {
+export default function Preview({ title, subtitle }) {
   // implement a Preview screen here
   //  Note - the HTML should have the following structure
   //  the main div should have a class of preview and the font-name prop
@@ -15,7 +15,7 @@ export default function Preview({ title }) {
   // </div>
   <div className="preview prop" style="text-align: center">
     <h1>{title}</h1>
-
+    <h3>{subtitle}</h3>
   </div>;
 
   return <></>;
